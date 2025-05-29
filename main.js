2e2edcbc9d95f9f61bd33d0a369c4e97d1dac79c
we-beep-byte <wwe1234566184@yahoo.com.tw>
2025-05-29 15:51:08 +0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,16 @@
+// demo.js - 一個簡單的 JavaScript 範例
+
+function greet(name) {
+  return `哈囉，${name}！`;
+}
+
+function add(a, b) {
+  return a + b;
+}
+
+function main() {
+  console.log(greet('世界'));
+  console.log(`3 + 5 = ${add(3, 5)}`);
+}
+
+main();
